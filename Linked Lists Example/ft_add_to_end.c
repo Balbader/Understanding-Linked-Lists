4da77daafb6ac956ac650f672861472767c43210
baalbade <baalbade@student.42.fr>
2022-11-21 16:31:14 +0100

    ft_add_to_end.c

--- Linked Lists Example/ft_add_to_end.c
+++ Linked Lists Example/ft_add_to_end.c
@@ -1,7 +1,4 @@
-// #include "ll.h"
-#include "stdio.h"
-#include <stdlib.h>
-#include "ft_create_node.c"
+#include "ll.h"
 
 void	ft_add_to_end(t_list **lst, t_list *node)
 {
@@ -16,20 +13,4 @@ void	ft_add_to_end(t_list **lst, t_list *node)
 			ptr = ptr->next;
 		ptr->next = node;
 	}
-}
-
-int main(void)
-{
-	t_list	*lst;
-	t_list	*node;	
-
-	lst = ft_create_node("First node");
-
-	node = ft_create_node("Last node");
-	// ft_add_to_end(&lst, node);
-	while (lst->next != NULL)
-		lst = lst->next;
-	lst->next = node;
-	printf("%s\n", (char *)lst->content);
-	return (0);
 }
