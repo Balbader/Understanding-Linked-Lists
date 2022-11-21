a09c346c95edb2e2523fad9d1f905b42ec1ba16a
baalbade <baalbade@student.42.fr>
2022-11-21 12:41:25 +0100

    rename variables and arguments

--- Linked Lists Example/ft_add_to_front.c
+++ Linked Lists Example/ft_add_to_front.c
@@ -4,19 +4,19 @@
 
 #include "ll.h"
 
-void	ft_add_to_front(t_list **lst, t_list *new_head)
+void	ft_add_to_front(t_list **lst, t_list *node)
 {
 	// Initialize the new_node that will be added to the top of the linked list
-	t_list *new_node;
+	t_list *new_head;
 
 	// Malloc size of t_list to new_node
-	new_node = (t_list *)malloc(sizeof(t_list));
-	if (!new_node)
+	new_head = (t_list *)malloc(sizeof(t_list));
+	if (!new_head)
 		return ;
 	
 	// Assign argument 'new_head' to new_node
-	new_node = new_head;
+	new_head = node;
 
 	// Make 1st pointer of lst point towards new_node
-	*lst = new_node;
+	*lst = new_head;
 }
